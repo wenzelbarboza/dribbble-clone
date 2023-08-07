440a021ebd6b5d1b193956df0b82e01f0e385fe6
wenzel audrin barboza <wenzelbarboza@gmail.com>
2023-08-07 08:40:18 +0300

    nav and footer styling implemented

---
+++ components/Footer.tsx
@@ -0,0 +1,89 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type footerItem = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: footerItem) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link, index) => (
+        <Link href="/" key={index}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
+
+const Footer = () => {
+  return (
+    <footer className=" flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="flexible"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint rerum
+            unde, itaque quidem quasi, aspernatur incidunt harum, illo minima
+            asperiores sapiente dignissimos omnis tenetur suscipit vel eius odit
+            quo eaque.
+          </p>
+        </div>
+        <div className="  flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+          <div className=" flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+          <div className=" flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
+          <div className=" flex-1 flex flex-col flex-wrap"></div>
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@2023 flexibble. All rights reserved</p>
+        <p className=" text-gray">
+          <span className=" text-black font-semibold">10,2014</span>
+          project submitted
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
